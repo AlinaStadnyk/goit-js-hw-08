20c1059bb77d55d80bf22e4b3f5f5157dabfbc4f
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-10-09 11:21:41 +0300

    added gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const selectors = {
+  gallery: document.querySelector('.gallery'),
+};
+
+selectors.gallery.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}"" alt="${description}" />
+   </a>
+</li>
+    `
+    )
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captions: true,
+  captionDelay: 250,
+  enableKeyboard: true,
+});
